ccd2aeac5d4e48ec6d65b420ba0553804cdd3ee1
Devesh <deveshamway@gmail.com>
2023-10-09 20:34:43 +0530

    Added funders and mappings

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,7 +2,41 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-  // storage variables
-  uint public funds = 1000; // positive values only
-  int public counter = -10;
+
+  uint public numOfFunders;
+  mapping(address => bool) private funders;
+  mapping(uint => address) private lutFunders;
+
+  receive() external payable {}
+
+  function addFunds() external payable {
+    address funder = msg.sender;
+
+    if (!funders[funder]) {
+      uint index = numOfFunders++;
+      funders[funder] = true;
+      lutFunders[index] = funder;
+    }
+  }
+
+  function getAllFunders() external view returns (address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+
+    for (uint i = 0; i < numOfFunders; i++) {
+      _funders[i] = lutFunders[i];
+    }
+
+    return _funders;
+  }
+
+  function getFunderAtIndex(uint8 index) external view returns(address) {
+    return lutFunders[index];
+  }
 }
+
+
+// const instance = await Faucet.deployed();
+// instance.addFunds({from: accounts[0], value: "200000000"})
+// instance.addFunds({from: accounts[1], value: "200000000"})
+// instance.getFunderAtIndex(0)
+// instance.getAllFunders()
